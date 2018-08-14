2418896dc0d71f4101acc6a6a86b98129d410107
riefive <rie_five@yahoo.com>
2018-08-14 13:11:34 +0700

    add sample 8

---
+++ sample8.js
@@ -0,0 +1,10 @@
+// Git-style sub-commands
+// file: ./examples/pm
+var program = require('commander');
+ 
+program
+    .version('0.1.0')
+    .command('install [name]', 'install one or more packages')
+    .command('search [query]', 'search with optional query')
+    .command('list', 'list packages installed', {isDefault: true})
+    .parse(process.argv);
